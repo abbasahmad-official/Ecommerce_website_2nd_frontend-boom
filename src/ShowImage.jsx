d1dbb25c2da8b5f0d5956258d1ae8e63b1984f77
Abbas Ahmad <abbasahmad8032@gmail.com>
2025-07-31 12:06:53 +0500

    small screen

--- src/ShowImage.jsx
+++ src/ShowImage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { API } from "./config"
-
+import "./css/home.css"
 
 const ShowImage = ({item, url, from=null, className="" }) => {
-   return (<div className={`product-img ${className}`}>
+   return (<div className={`responsive-image ${className}`}>
         <img className={`${className}`} src={`${API}/${url}/photo/${item._id}`}  alt={item.name} style={{ objectFit: "cover", height: from? "70px": "200px"}}/>
     </div>)
 }
