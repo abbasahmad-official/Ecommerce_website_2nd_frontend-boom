5f0f83bb385f78fd535242698fc1bc0f911e7392
Abbas Ahmad <abbasahmad8032@gmail.com>
2025-07-31 11:14:48 +0500

    image update

--- src/ShowImage.jsx
+++ src/ShowImage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { API } from "./config"
 
+
 const ShowImage = ({item, url, from=null, className="" }) => {
    return (<div className={`product-img ${className}`}>
         <img className={`${className}`} src={`${API}/${url}/photo/${item._id}`}  alt={item.name} style={{ objectFit: "cover", height: from? "70px": "200px"}}/>
